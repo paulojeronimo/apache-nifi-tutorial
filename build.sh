bada876404901a682fc19aab8f2f8741d78b04ba
Paulo Jerônimo <paulojeronimo@gmail.com>
2018-05-02 14:26:33 +0100

    README.adoc updates and build.sh refactoring

--- build.sh
+++ build.sh
@@ -1,29 +1,70 @@
 #!/usr/bin/env bash
+set -e
+
+DEBUG=${DEBUG:-false}
+$DEBUG && set -x || set +x
 
 BASE_DIR=${BASE_DIR:-`cd "$(dirname "$0")"; pwd`}
-cd "$BASE_DIR"
 
-build_dir=build
-build_docs_dir=$build_dir/asciidoc
-html_dir=$build_docs_dir/html5
-pdf_dir=$build_docs_dir/pdf
-pdf_name=apache-nifi-tutorial.pdf
+config=config
+[ -r $config ] || config=$config.sample
+source "$BASE_DIR/$config"
 
-doc=src/docs/asciidoc/index.adoc
-attrs="-a projectdir=$BASE_DIR"
+_README() {
+  asciidoctor README.adoc
+}
 
 _build() {
-  asciidoctor $attrs -D "$html_dir" $doc
+  asciidoctor $asciidoctor_attrs -D "$html_dir" $doc
   ( cd src/docs/asciidoc; rsync -a images "$BASE_DIR/$html_dir/" )
 }
 
 _pdf() {
-  asciidoctor-pdf $attrs -o $pdf_name -D "$pdf_dir" $doc
+  asciidoctor-pdf $asciidoctor_attrs -o $pdf_name -D "$pdf_dir" $doc
 }
 
 _clean() {
   rm -rf $build_dir
 }
 
+_publish() {
+  local published_dir=`mktemp -d`
+  local remote_repo=`git config --get remote.origin.url`
+
+  if [ ".$remote_repo" = "." ]
+  then
+    echo 'The remote repository is not configured!'
+    return 1
+  fi
+
+  mkdir -p "$published_dir"
+
+  cd build/asciidoc/html5
+  rsync -am \
+    --include='**/' \
+    --include='/index.html' \
+    --include='/images/**' \
+    --exclude='*' \
+    . "$published_dir"/
+  cd - &> /dev/null
+
+  cd build/asciidoc/pdf
+  [ -r $pdf_name ] || cp index.pdf $pdf_name
+  cp $pdf_name "$published_dir"/
+  cd - &> /dev/null
+
+  tree -a "$published_dir"
+
+  local msg="Published at `date`"
+  cd "$published_dir"
+  git init
+  git add -A
+  git commit -m "$msg"
+  git push --force $remote_repo master:gh-pages
+  cd - &> /dev/null
+}
+
 op=${1:-build}
-type _$op &> /dev/null && _$op || true
+cd "$BASE_DIR"
+type _$op &> /dev/null || exit 1
+_$op
